28b31b9bd346253b3a37226dc559f1644839ac13
204G1A05B0 <112165506+204G1A05B0@users.noreply.github.com>
2022-09-23 15:52:13 +0530

    Update fccfs.c

--- OSlab/fccfs.c
+++ OSlab/fccfs.c
@@ -16,9 +16,8 @@ wt[0] = wtavg = 0;
 tat[0] = tatavg = bt[0];
 for(i=1;i<n;i++)
 {
-
-wt[i] = wt[i-1] +bt[i-1];
-tat[i] = tat[i-1] +bt[i];
+wt[i] = wt[i-1] + bt[i-1];
+tat[i] = tat[i-1] + bt[i];
 wtavg = wtavg + wt[i];
 tatavg = tatavg + tat[i];
 }
