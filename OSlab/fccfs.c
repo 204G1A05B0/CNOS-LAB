fbdee4ccc0e8e7d46473b4c04b046c07dc018c2b
204G1A05B0 <112165506+204G1A05B0@users.noreply.github.com>
2022-09-30 15:24:22 +0530

    Update fccfs.c

--- OSlab/fccfs.c
+++ OSlab/fccfs.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
-#include<conio.h>
 main()
 {
 int bt[20], wt[20], tat[20], i, n;
 float wtavg, tatavg;
-clrscr();
 printf("\nEnter the number of processes -- ");
 scanf("%d", &n);
 for(i=0;i<n;i++)
@@ -26,5 +24,4 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n);
-getch();
 }
