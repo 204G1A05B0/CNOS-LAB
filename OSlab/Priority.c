a69b8fb3f9950c828272366ea9bb20044ecdf2c4
204G1A05B0 <112165506+204G1A05B0@users.noreply.github.com>
2022-11-02 21:16:39 +0530

    Update Priority.c

--- OSlab/Priority.c
+++ OSlab/Priority.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 main()
 {
-int p[20],bt[20],pri[20], wt[20],tat[20],i, k, n, temp; float wtavg,
-tatavg;
+int p[20],bt[20],pri[20], wt[20],tat[20],i, k, n, temp; 
+float wtavg,tatavg;
 printf("Enter the number of processes --- ");
 scanf("%d",&n);
 for(i=0;i<n;i++){
 p[i] = i;
-printf("Enter the Burst Time & Priority of Process %d --- ",i); scanf("%d
-%d",&bt[i], &pri[i]);
+printf("Enter the Burst Time & Priority of Process %d --- ",i); 
+scanf("%d %d",&bt[i], &pri[i]);
 }
 for(i=0;i<n;i++)
 for(k=i+1;k<n;k++)
@@ -32,11 +32,8 @@ tat[i] = tat[i-1] + bt[i];
 wtavg = wtavg + wt[i];
 tatavg = tatavg + tat[i];
 }
-printf("\nPROCESS\t\tPRIORITY\tBURST TIME\tWAITING TIME\tTURNAROUND
-TIME");
+printf("\nPROCESS\t\tPRIORITY\tBURST TIME\tWAITING TIME\tTURNAROUND TIME");
 for(i=0;i<n;i++)
 printf("\n%d \t\t %d \t\t %d \t\t %d \t\t %d ",p[i],pri[i],bt[i],wt[i],tat[i]);
-
-printf("\nAverage Waiting Time is --- %f",wtavg/n); printf("\nAverage
-Turnaround Time is --- %f",tatavg/n);
+printf("\nAverage Waiting Time is --- %f",wtavg/n); printf("\nAverage Turnaround Time is --- %f",tatavg/n);
 }
